0c49aefecc80ae6448ee7730a0e6dd702fab0310
Trần Minh Quân <76668715+MinhquanDevSoldier@users.noreply.github.com>
2021-07-19 04:20:41 +0700

    Update layout.php

--- application/views/frontend/layout.php
+++ application/views/frontend/layout.php
@@ -12,6 +12,9 @@
             if(isset($title))
                 echo $title;
             else
+                echo "
+                    <script>alert("Website được tạo ra nhằm phục vụ môn học Lập trình mã nguồn mỡ vui lòng không xem đây là trang thương mại điện tử")</script>
+                ";
                 echo "Smart Store - Điện thoại, Laptop, Link kiện chính hãng";
         ?>
     </title>
